2c8ed8e7cd830451aa64006f5b63021037ca0b0a
shubh <shubhamkarale17@gmail.com>
2021-05-30 12:49:03 +0530

    first commit

---
+++ Shubham_k/src/test/java/Demo.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author shubh
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	
+	@Test
+	public void test(){
+		
+		System.out.println("Hello world");
+		
+	}
+}
